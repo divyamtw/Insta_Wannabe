d4855909f17cb02b8dbded0ec393a533793edb26
Divyam Tiwari <tiwarixdivyam@gmail.com>
2026-02-26 13:37:38 +0530

    react router setup

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,7 @@
-
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app.routes.jsx";
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
